a578d248a083e2c2cb11ec65ff9630d577094bef
Hima Shukla <himashukla49@gmail.com>
2021-08-10 11:51:28 +0530

    gitignore working

--- custom/modules/Accounts/Ext/Language/en_us.lang.ext.php
+++
@@ -1,8 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
-//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
-$mod_strings['LBL_ACCOUNTS_FP_EVENT_LOCATIONS_1_FROM_FP_EVENT_LOCATIONS_TITLE'] = 'Locations';
-
-?>
