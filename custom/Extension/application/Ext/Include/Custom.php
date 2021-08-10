5b9e17cc13df44c7edd026d598706525262d39a1
Hima Shukla <himashukla49@gmail.com>
2021-08-10 15:25:57 +0530

    Custom Module Created | Custom Package: Custom|Custom Module: Family

---
+++ custom/Extension/application/Ext/Include/Custom.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['cs_Family'] = 'cs_Family';
+$beanFiles['cs_Family'] = 'modules/cs_Family/cs_Family.php';
+$moduleList[] = 'cs_Family';
+
+?>
