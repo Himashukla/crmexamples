cf577e0a355380dcaa6748d954197f6fab623d5e
Hima Shukla <himashukla49@gmail.com>
2021-08-19 14:43:58 +0530

    field added in basic filter

---
+++ custom/modules/cs_Family/metadata/searchdefs.php
@@ -0,0 +1,65 @@
+<?php
+$module_name = 'cs_Family';
+$searchdefs [$module_name] = 
+array (
+  'layout' => 
+  array (
+    'basic_search' => 
+    array (
+      'name' => 
+      array (
+        'name' => 'name',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'current_user_only' => 
+      array (
+        'name' => 'current_user_only',
+        'label' => 'LBL_CURRENT_USER_FILTER',
+        'type' => 'bool',
+        'default' => true,
+        'width' => '10%',
+      ),
+      'cs_family_fp_event_locations_1_name' => 
+      array (
+        'type' => 'relate',
+        'link' => true,
+        'label' => 'LBL_CS_FAMILY_FP_EVENT_LOCATIONS_1_FROM_FP_EVENT_LOCATIONS_TITLE',
+        'id' => 'CS_FAMILY_FP_EVENT_LOCATIONS_1FP_EVENT_LOCATIONS_IDB',
+        'width' => '10%',
+        'default' => true,
+        'name' => 'cs_family_fp_event_locations_1_name',
+      ),
+    ),
+    'advanced_search' => 
+    array (
+      0 => 'name',
+      1 => 
+      array (
+        'name' => 'assigned_user_id',
+        'label' => 'LBL_ASSIGNED_TO',
+        'type' => 'enum',
+        'function' => 
+        array (
+          'name' => 'get_user_array',
+          'params' => 
+          array (
+            0 => false,
+          ),
+        ),
+      ),
+    ),
+  ),
+  'templateMeta' => 
+  array (
+    'maxColumns' => '3',
+    'maxColumnsBasic' => '4',
+    'widths' => 
+    array (
+      'label' => '10',
+      'field' => '30',
+    ),
+  ),
+);
+;
+?>
